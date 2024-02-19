239a61c221ba44414ea25935a857e436cda2cae9
Jérémy Riverain <jeremy@geekco.fr>
2024-02-19 17:27:25 +0100

    ✨ add event channel demo for android

--- app/android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.playgroundnative
 
+import androidx.annotation.NonNull
+import com.example.batterylevel.TimeHandler
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val timerEventChannel = "flutter.demo/timer"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, timerEventChannel).setStreamHandler(
+            TimeHandler
+        )
+    }
 }
